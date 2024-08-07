54d57fc90c8dcdc83763da19b4d38644c113b1b1
ipa1987 <idris.patel1987@outlook.com>
2024-08-07 23:06:24 +0100

    problem 1 solved

---
+++ CodingAssignmentsWeek1/Collatz.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingAssignmentsWeek1
+{
+    public class Collatz
+    {
+        public static string CompareCollatzSteps(int a, int b)
+        {
+            int stepsA = CountCollatzSteps(a);
+            int stepsB = CountCollatzSteps(b);
+
+            return stepsA < stepsB ? "a" : "b";
+        }
+
+        // private function below to separate main function of solution which is declared above for a and b integers, hence runs twice for comparison
+
+        private static int CountCollatzSteps(int n)
+        {
+            int steps = 0;
+
+            while (n != 1)
+            {
+                // if n is divisible by 2, use modulo, %
+                if (n % 2 == 0)
+                {
+                    // similar to increment/decrement, but dividing
+                    n /= 2;
+                }
+                else
+                {
+                    n = (3 * n) + 1;
+                }
+
+                // increments by +1
+                steps++;
+            }
+
+            return steps;
+        }
+    }
+}
--- CodingAssignmentsWeek1/Program.cs
+++ CodingAssignmentsWeek1/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Please Enter the first positive integer (a): ");
+            int a = int.Parse(Console.ReadLine()!);
+
+            Console.Write("Please Enter the second positive integer (b): ");
+            int b = int.Parse(Console.ReadLine()!);
+
+            string result = Collatz.CompareCollatzSteps(a, b);
+            Console.WriteLine($"The integer that takes fewer steps to reach 1 is: {result}");
         }
     }
 }
