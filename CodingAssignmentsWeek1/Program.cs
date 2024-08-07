63cbbca5e14f130acdd7f50498ef0d5d9717f453
ipa1987 <idris.patel1987@outlook.com>
2024-08-07 23:57:50 +0100

    problem 2 solved

--- CodingAssignmentsWeek1/Program.cs
+++ CodingAssignmentsWeek1/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            // Test CollatzSequence
             Console.Write("Please Enter the first positive integer (a): ");
             int a = int.Parse(Console.ReadLine()!);
 
@@ -12,6 +13,15 @@
 
             string result = Collatz.CompareCollatzSteps(a, b);
             Console.WriteLine($"The integer that takes fewer steps to reach 1 is: {result}");
+
+            // Test ReverseAndConcatenate
+            Console.Write("Enter an integer to reverse and concatenate: ");
+            int numberToReverse = int.Parse(Console.ReadLine()!);
+            string reversedResult = ReverseAndConcatenate.Perform(numberToReverse);
+            Console.WriteLine($"Reversed and concatenated result: {reversedResult}");
+
+
+
         }
     }
 }
---
+++ CodingAssignmentsWeek1/ReverseAndConcatenate.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingAssignmentsWeek1
+{
+    public class ReverseAndConcatenate
+    {
+        public static string Perform(int i)
+        {
+            return new string(i.ToString().Reverse().ToArray()) + i;
+        }
+    }
+}
